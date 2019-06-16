75e105c3316121b1a3c88a9c245bb70e9e31e2e7
usherasnick <461826470@qq.com>
2019-06-16 15:54:09 +0800

    Common utilies

---
+++ internal/delay-queue/common.go
@@ -0,0 +1,8 @@
+package delayqueue
+
+const (
+	DefaultBucketName                    = "delay_queue_bucket"
+	DefaultReadyQueueName                = "delay_queue_ready_queue"
+	DefaultTopicSetName                  = "delay_queue_topic_set"
+	DefaultBlockPopFromReadyQueueTimeout = 10
+)
